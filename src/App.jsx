364fb03146273f82973aa66edd4eddc5c70de81a
Jorge Escalante <jorgealberto9504@gmail.com>
2024-05-16 15:42:33 -0600

    rentrega

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,10 @@
 /* import styles from "./App.module.css" */  /* al momento de agregar nuestro archivo css debemos de importarlo de esta forma en la parte superior que querramos que sea afectada por este archivo css */
 import { Home } from "./Pages/Home/Home.jsx"
-import { Contact } from "./Pages/Contact/Contact.jsx"
 
 const App = () => {
   return (
     <>    
        <Home/> 
-     <Contact/>
 
  {/* Aqui usamos sugar syntax usando spread operator */
 
